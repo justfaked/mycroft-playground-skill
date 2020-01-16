4cc5f9fafb2c46be72c4ded5ee39b24dd9d25ba9
Kilb, Philipp <philipp.kilb@outlook.de>
2020-01-16 12:34:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/adjust.svg" card_color="#BFBFBF" width="50" height="50" style="vertical-align:bottom"/> Mycroft Playground
+Testing
+
+## About
+Testing
+
+## Examples
+* "Lets play"
+* "Play with me"
+* "Play"
+
+## Credits
+cp
+
+## Category
+**IoT**
+
+## Tags
+
---
+++ __init__.py
@@ -0,0 +1,15 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class MycroftPlayground(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('playground.mycroft.intent')
+    def handle_playground_mycroft(self, message):
+        self.speak_dialog('playground.mycroft')
+
+
+def create_skill():
+    return MycroftPlayground()
+
